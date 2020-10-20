ff8470fd5490f4910631e5bf8153bd97f19c9c41
Trent Jones <trent-j@github.com>
2020-10-20 19:04:16 +0000

    example project

---
+++ src/test/java/com/github/trentj/jdconf/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package com.github.trentj.jdconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+
+        MockHttpServletRequestBuilder helloRequest = MockMvcRequestBuilders
+                .get("/")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc.perform(helloRequest)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
+    }
+}
